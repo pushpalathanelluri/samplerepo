9a86170dda18dac19bd80132cf3358c466061d75
Ratna.paleru <Ratna.paleru@milliman.com>
2024-08-05 11:58:23 -0700

    added

---
+++ src/webparts/repo/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IRepoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'RepoWebPartStrings' {
+  const strings: IRepoWebPartStrings;
+  export = strings;
+}
